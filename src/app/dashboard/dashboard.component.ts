bd4091d391de7815c27401c4335a07de27cf8319
Sanyam Saxena <sanyamsxn1998@gmail.com>
2025-04-20 17:40:28 +0530

    dashboard layout ready

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,13 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { FilesListsComponent } from './files-lists/files-lists.component';
 import { FiltersComponent } from './filters/filters.component';
+import { HeaderComponent } from '../header/header.component';
+import { HttpClient } from '@angular/common/http';
+
+
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [FilesListsComponent, FiltersComponent],
+  imports: [FilesListsComponent, FiltersComponent,HeaderComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
 
+
+export class DashboardComponent implements OnInit{
+  private http = inject(HttpClient);
+  files: any[] = [];
+
+  ngOnInit(): void {
+    
+  }
+
+  //load most downloaded files
+  loadMostDownloaded(): void {
+    this.http.get<any[]>('http://localhost:3000/api/files/most-downloaded').subscribe({
+      next: (res) => {
+        this.files = res;
+      },
+      error: (err) => {
+        console.error('Failed to load most downloaded files:', err);
+      }
+    });
+  }
 }
--- src/app/dashboard/filters/filters.component.ts
+++ src/app/dashboard/filters/filters.component.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
-
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-filters',
-  standalone: true,
-  imports: [],
+  standalone:true,
+  imports:[CommonModule],
   templateUrl: './filters.component.html',
   styleUrl: './filters.component.css'
 })
 export class FiltersComponent {
+  courseList = [
+    'MCA',
+    'BTech-CS',
+    'BCA',
+    'BBA',
+    'MTech',
+    'BTech-EEE',
+    'BTech-IT'
+  ];
 
+  selectedCourses: string[] = [];
+
+  @Output() filtersChanged = new EventEmitter<string[]>();
+
+  onCheckboxChangeEvent(event: Event, course: string) {
+    const input = event.target as HTMLInputElement;
+    const checked = input.checked;
+  
+    if (checked && !this.selectedCourses.includes(course)) {
+      this.selectedCourses.push(course);
+    } else if (!checked) {
+      this.selectedCourses = this.selectedCourses.filter(c => c !== course);
+    }
+  
+    this.filtersChanged.emit(this.selectedCourses);
+  }
+  
 }
